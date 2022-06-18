bcae5d22e3aad7c7742e1b9a17c8aadec24a205b
SyerAru <syeraru@gmail.com>
2022-06-17 21:38:47 -0600

    Added for loop so game ends after 5 rounds

--- script.js
+++ script.js
@@ -24,55 +24,71 @@ function computerTurn() {
 
 }
 
+let playerCount = 0;
+let computerCount = 0;
+
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerCount += 1;
+        console.log("Your score is " + playerCount + ". Computer's score is " + computerCount + ".");
         return "You win! Rock beats scissors.";
+
     } else if (
         playerSelection === "paper" && computerSelection === "rock"
     ) {
+        playerCount += 1;
+        console.log("Your score is " + playerCount + ". Computer's score is " + computerCount + ".");
         return "You win! Paper beats rock.";
     } else if (
         playerSelection === "scissors" && computerSelection === "paper"
     ) {
+        playerCount += 1;
+        console.log("Your score is " + playerCount + ". Computer's score is " + computerCount + ".");
         return "You win! Scissors beats paper.";
     } else if (
         playerSelection === "scissors" && computerSelection === "rock"
     ) {
+        computerCount += 1;
+        console.log("Your score is " + playerCount + ". Computer's score is " + computerCount + ".");
         return "You lose! Rock beats scissors.";
     } else if (
         playerSelection === "rock" && computerSelection === "paper"
     ) {
+        computerCount += 1;
+        console.log("Your score is " + playerCount + ". Computer's score is " + computerCount + ".");
         return "You lose! Paper beats rock.";
     } else if (
         playerSelection === "paper" && computerSelection === 'scissors'
     ) {
+        computerCount += 1;
+        console.log("Your score is " + playerCount + ". Computer's score is " + computerCount + ".");
         return "You lose! Scissors beats paper.";
     } else {
+        console.log("Your score is " + playerCount + ". Computer's score is " + computerCount + ".");
         return "It's a tie!";
     }
 
 }
 
-console.log(playRound(playerTurn(), computerTurn()));
-
-
 function game() {
     for (let i = 0; i < 5; i++) {
         console.log(playRound(playerTurn(), computerTurn()));
     }
-    
+
+    if (playerCount > computerCount) {
+        console.log("You win the match!");
+    } else if (playerCount < computerCount) {
+        console.log("Computer wins the match!");
+    } else {
+        console.log("It's a tie!");
+    }
 }
 
-console.log(game())
-// computerTurn();
-// console.log(computerSelection);
-// playerTurn();
-// console.log(playerSelection);
-// playRound(computerSelection, playerSelection);
+game();
 
-
-//>>>>Simplifed Version
+//>>>>Simplifed Version for playRound()
+// 
 // switch(playerSelection, computerSelection) {
 //     case playerSelection === "rock" && computerSelection === "scissors":
 //     case playerSelection === "paper" && computerSelection === "rock":
